d507519f0f5718913c4ebff6390bfd4bd234b0e1
Marc Mills <mdmills1990@gmail.com>
2018-10-25 13:55:31 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   INNER JOIN characters
   ON characters.series_id = series.id
-  GROUP BY series.title HAVING COUNT(characters.species) DESC LIMIT 1;"
+  GROUP BY series.title HAVING COUNT(characters.species);"
 end
 
 def select_character_names_and_number_of_books_they_are_in
