081bef2576004a95a02494bfe8aad2826cd107df
Marc Mills <mdmills1990@gmail.com>
2018-10-25 13:37:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,9 +18,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series.subgenre_id FROM series
-  INNER JOIN authors
-  ON series.author_id = authors.id;"
+  "SELECT authors.name, subgenres.name FROM authors
+  INNER JOIN series
+  ON series.author_id = authors.id
+  INNER JOIN subgenres
+  on series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
