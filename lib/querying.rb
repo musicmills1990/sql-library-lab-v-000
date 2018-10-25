bf900bfd356aadb2cca944ab517529c80487ddca
Marc Mills <mdmills1990@gmail.com>
2018-10-25 13:58:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   INNER JOIN characters
   ON characters.series_id = series.id
-  GROUP BY series.title
+  ORDER BY series.title
   WHERE characters.species = 'human';"
 end
 
