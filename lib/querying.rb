8056cb3f15b83b9e9fd481f6e2de38a58c54f08e
Marc Mills <mdmills1990@gmail.com>
2018-10-25 13:52:44 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,11 +22,13 @@ def select_name_and_series_subgenres_of_authors
   INNER JOIN series
   ON series.author_id = authors.id
   INNER JOIN subgenres
-  on series.subgenre_id = subgenres.id;"
+  ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+  INNER JOIN characters
+  ON characters.series_id = series.id;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
